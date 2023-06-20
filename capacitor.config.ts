ea9502f73b4b30bd0533fd50856079b924bfb0d7
Mahdi Zarei <mahdizarei0614@gmail.com>
2023-06-20 17:21:26 +0330

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'fateh-safar-ionic',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
